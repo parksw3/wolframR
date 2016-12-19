248f1f7912beb8c54278e80a954fee0a737b3831
parksw3 <parksw3@mcmaster.ca>
2016-12-19 02:27:28 -0500

    bit more progress

--- R/nest.R
+++ R/nest.R
@@ -7,23 +7,12 @@
 ##' ## fibonacci sequence
 ##' nest(c(x, x[length(x)] + x[length(x)-1]), c(1, 1), 5)
 ##' @export
-nest <- function(expr, x, n, xname = "x") {
-    sexpr <- substitute(expr)
-    res <- nest_base(sexpr, substitute(x), n, xname)
-    res[[length(res)]]
-}
-
-##' @export
-nest_list <- function(expr, x, n, xname = "x"){
-    sexpr <- substitute(expr)
-    nest_base(sexpr, substitute(x), n, xname)
-}
-
-nest_base <- function(expr, x, n, xname = "x") {
+nest <- function(expr, x, n, m = 1, xname = "x") {
     if(n < 2 || n %% 1 != 0){
         stop("'n' needs to be an integer greater than 1")
     }
-    expr <- get_expression(expr, xname)
+    x <- substitute(x)
+    expr <- get_expression(substitute(expr), xname)
     ssexpr <- substitute(substitute(expr))
     res <- vector("list", n)
     arg_base <- arg <- alist(x = )
@@ -31,7 +20,7 @@ nest_base <- function(expr, x, n, xname = "x") {
 
     arg[[1]] <- x
     eval_base <- try(eval(eval(ssexpr, envir = arg, enclos = parent.frame())), silent = TRUE)
-    eval_logi <- ifelse(!is(eval_base, "try-error"), TRUE, FALSE)
+    eval_logi <- !is(eval_base, "try-error")
 
     if(!eval_logi){
         if (length(as.character(x)) > 1){
@@ -40,7 +29,6 @@ nest_base <- function(expr, x, n, xname = "x") {
             names(arg_base) <- as.character(x)
         }
     }
-
 
     for(i in 1:n){
         arg[[1]] <- x <- eval(ssexpr, envir = arg, enclos = parent.frame())
@@ -51,9 +39,41 @@ nest_base <- function(expr, x, n, xname = "x") {
             res[[i]] <- make_function(arg_base, x, .GlobalEnv)
         }
     }
-    res
+
+    v <- unlist(lapply(res, length))
+
+    if(all(v == 1) & is.vector(res[[1]]))
+        res <- unlist(res)
+
+    if(m == "all" | m > n)
+        m <- n
+
+    return(res[(n-m+1):n])
 }
 
-nest_while <- function(expr, x, cond, xname = "x"){
+##' repeat until the condition is met
+##'
+##' @examples
+##' ## collatz sequence
+##' nest_while(c(x, ifelse(x[length(x)] %% 2 == 0, x[length(x)]/2, 3*x[length(x)]+1)), 1214, x[length(x)] != 1)
+##' @export
+nest_while <- function(expr, x, cond, m = 1, max = NA, n = 0, xname = "x"){
+    sexpr <- substitute(expr)
+    expr <- get_expression(sexpr)
+    scond <- substitute(cond)
+    cond <- get_expression(scond, varname = "'cond'")
+    cond_base <- try(eval(cond, envir = list(x = x), enclos = parent.frame()), silent = TRUE)
+    if(is(eval_base, "try-error"))
+        stop(gettextf("'cond  cannot be evaluated at '%s'.", x), domain = NA)
 
+    if(!is.logical(cond_base))
+        stop("'cond' must return TRUE/FALSE.")
+
+    res <- vector("list", 100)
+    i <- 1
+
+    while(eval(cond, envir = list(x = x), enclos = parent.frame())){
+        x <- eval(expr, envir = list(x = x), enclos = parent.frame())
+    }
+    x
 }
